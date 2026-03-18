957193f469b62f577f01e46a4f47a7839d1c1442
casey-brooks <casey.brooks@agyn.io>
2026-03-18 00:12:43 +0000

    feat: add Playwright e2e tests (#9)
    * feat(e2e): add playwright tests
    
    * fix(e2e): align playwright image

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import { mockApiPlugin } from './vite-plugin-mock-api';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), mockApiPlugin()],
+  test: {
+    exclude: [...configDefaults.exclude, 'test/e2e/**'],
+  },
   server: {
     allowedHosts: true,
     proxy: {
